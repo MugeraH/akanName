e36af983bb40a337f9391e5776488e6d4694f9e0
MugeraH <bwosihughes24@gmail.com>
2021-02-20 11:34:11 +0300

    add validation rule for date, for user not to select date before today

--- js/index.js
+++ js/index.js
@@ -6,11 +6,21 @@ userForm.addEventListener("submit", (e)=>{
   e.preventDefault()
 
   date = document.getElementById('date').value
-
-
-
   gender = document.querySelector('input[name="genderRadio"]:checked').value;
-  myModal.show()
+  //? will output month
+  // myDate = (new Date(date)).getMonth() +1;
+  dateToday = (new Date()).getDate()  
+myDate = (new Date(date)).getDate()  
+
+if(myDate > dateToday){
+console.log("PLease selcet valid date,you can not be born before today");
+}else{
+  console.log(myDate,dateToday,gender);
+}
+
+
+
+  //myModal.show()
   
-  console.log(date,gender);
+
 })
